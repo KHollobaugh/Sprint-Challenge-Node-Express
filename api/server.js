975bd3a5037ea746a5b4e0e1db84e9df43c5a762
KHollobaugh <khollobaugh@gmail.com>
2018-11-09 13:23:07 -0500

    added cors to display data

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const projectsDb = require('../data/helpers/projectModel');
 const actionsDb = require('../data/helpers/actionModel');
+const cors = require('cors');
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.json('alive');
